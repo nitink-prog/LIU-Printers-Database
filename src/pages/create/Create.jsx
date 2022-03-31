c8f9f4fc3c1d1dd681c868505df83f7b4df179f8
nitink-prog <80429955+nitink-prog@users.noreply.github.com>
2022-03-31 15:40:20 -0400

    create function working

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -22,7 +22,6 @@ export default function Create() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const printer = {
-      id: ipAddress,
       building,
       dateUpdated,
       department,
@@ -33,84 +32,116 @@ export default function Create() {
       serial,
     };
     try {
-      await db.collection("recipes").add(recipe);
+      await db.collection("printers").doc(ipAddress).set(printer);
       history.push("/");
     } catch (err) {
       console.log(err);
     }
   };
 
-  const handleAdd = (e) => {
-    e.preventDefault();
-    const trimmedIngredient = newIngredient.trim();
-
-    if (trimmedIngredient && !ingredients.includes(trimmedIngredient)) {
-      setIngredients((prevIngredients) => [
-        ...prevIngredients,
-        trimmedIngredient,
-      ]);
-    }
-    setNewIngredient("");
-    ingredientInput.current.focus();
-  };
-
   return (
     <div className={`create ${mode}`}>
       <h2 className="page-title">Add a Printer</h2>
-
       <form onSubmit={handleSubmit}>
+        <h4>Required</h4>
         <label>
-          <span>Recipe title:</span>
+          <span>IP Address (Last Octet only):</span>
+          <input
+            required
+            type="number"
+            min="100"
+            max="255"
+            onChange={(e) => setIpAddress(e.target.value)}
+            value={ipAddress}
+          />
+        </label>
+
+        <label>
+          <span>Department </span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setDepartment(e.target.value)}
+            value={department}
+          />
+        </label>
+
+        <label>
+          <span>Building </span>
+          <select
+            required
+            onChange={(e) => setBuilding(e.target.value)}
+            value={building}>
+            <option value="DEFAULT" disabled>
+              Select...
+            </option>
+            <option value="H">H - Humanities</option>
+            <option value="HS">HS - Health Sciences</option>
+            <option value="L">L - Pharmacy</option>
+            <option value="LLC">LLC - Library Learning Center</option>
+            <option value="M">M - Metcalfe</option>
+            <option value="P">P - Pratt</option>
+            <option value="S">S - Sloan</option>
+          </select>
+        </label>
+
+        <label>
+          <span>Room </span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setRoom(e.target.value)}
+            value={room}
+          />
+        </label>
+
+        <label>
+          <span>Model </span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setModel(e.target.value)}
+            value={model}
+          />
+        </label>
+
+        <h4>Advanced (Optional)</h4>
+        <label>
+          <span>Name </span>
           <input
             type="text"
-            onChange={(e) => setIpaddress(e.target.value)}
-            value={ipaddress}
-            required
+            onChange={(e) => setName(e.target.value)}
+            value={name}
           />
         </label>
 
         <label>
-          <span>Recipe ingredients:</span>
-          <div className="ingredients">
-            <input
-              type="text"
-              onChange={(e) => setNewIngredient(e.target.value)}
-              value={newIngredient}
-              ref={ingredientInput}
-            />
-            <button
-              onClick={handleAdd}
-              className="btn"
-              style={{ backgroundColor: color }}>
-              Add
-            </button>
-          </div>
-        </label>
-        <p>
-          Added:{" "}
-          {ingredients.map((i) => (
-            <em key={i}>{i}, </em>
-          ))}
-        </p>
-
-        <label>
-          <span>Recipe method:</span>
-          <textarea
-            onChange={(e) => setMethod(e.target.value)}
-            value={method}
-            required
-          />
-        </label>
-
-        <label>
-          <span>Cooking time (minutes):</span>
+          <span>MAC Address </span>
           <input
-            type="number"
-            onChange={(e) => setCookingTime(e.target.value)}
-            value={cookingTime}
-            required
+            type="text"
+            onChange={(e) => setMacAddress(e.target.value)}
+            value={macAddress}
           />
         </label>
+
+        <label>
+          <span>Serial </span>
+          <input
+            type="text"
+            onChange={(e) => setSerial(e.target.value)}
+            value={serial}
+          />
+        </label>
+
+        <label>
+          <span>Date Updated </span>
+          <input
+            type="date"
+            onChange={(e) => setDateUpdated(e.target.value)}
+            value={dateUpdated}
+          />
+        </label>
+
         <button
           className="btn"
           style={{ backgroundColor: color }}
